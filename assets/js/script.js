229a210cd3ca1bde2b71506348ac737aea1cbea5
napling <matthewcellis98@gmail.com>
2024-08-26 01:39:43 -0500

    draggable is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,23 +65,23 @@ function renderTaskList() {
             done.append(createTaskCard(task));
         }
     }
-console.log(taskList)
-    // // ? Use JQuery UI to make task cards draggable
-    // $('.draggable').draggable({
-    //     opacity: 0.7,
-    //     zIndex: 100,
-    //     // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
-    //     helper: function (e) {
-    //         // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
-    //         const original = $(e.target).hasClass('ui-draggable')
-    //             ? $(e.target)
-    //             : $(e.target).closest('.ui-draggable');
-    //         // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
-    //         return original.clone().css({
-    //             width: original.outerWidth(),
-    //         });
-    //     },
-    // });
+    console.log(taskList)
+    // ? Use JQuery UI to make task cards draggable
+    $('.draggable').draggable({
+        opacity: 0.7,
+        zIndex: 100,
+        // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+        helper: function (e) {
+            // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+            const original = $(e.target).hasClass('ui-draggable')
+                ? $(e.target)
+                : $(e.target).closest('.ui-draggable');
+            // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+            return original.clone().css({
+                width: original.outerWidth(),
+            });
+        },
+    });
     // location.reload();
 }
 
@@ -128,12 +128,29 @@ function handleDeleteTask(event) {
     console.log(filteredTasks);
     localStorage.setItem('tasks', JSON.stringify(filteredTasks));
     location.reload();
-    loadTasks();
+    // loadTasks();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-    const taskId = ui.draggable[0].dataset.id
+    // ? Get the project id from the event
+    const id = ui.draggable[0].getAttribute('data-task-id');
+console.log(id);
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+    // ? Get the id of the lane that the card was dropped into
+    const newStatus = event.target.id;
+    console.log(newStatus);
+
+    for (let task of tasks) {
+        // ? Find the project card by the `id` and update the project status.
+        if (task.id == id) {
+            task.status = newStatus;
+        }
+    }
+    // ? Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+// loadTasks();
+location.reload();
 }
 
 taskForm.on('submit', handleAddTask)
@@ -141,6 +158,16 @@ taskForm.on('submit', handleAddTask)
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    $('#task-due').datepicker({
+        changeMonth: true,
+        changeYear: true,
+    });
+    
+    // ? Make lanes droppable
+    $('.lane').droppable({
+        accept: '.draggable',
+        drop: handleDrop,
+    });
 });
 
 const loadTasks = function () {
